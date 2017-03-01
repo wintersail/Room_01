2ce8639e72d9430dd68092a2ad09a69f88e66925
wintersail <wintersail1992@outlook.com>
2017-03-01 17:20:35 +0800

    Add files via upload

---
+++ 2017022801/requestHandlers.js
@@ -0,0 +1,50 @@
+var exec = require("child_process").exec;
+var querystring = require("querystring");
+var fs = require("fs");
+
+function start(response) {
+    console.log("You visit /star");
+    var body = '<html>' + '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+response.writeHead(200, {"Content-Type": "text/html"});
+response.write(body);
+response.end();
+}
+
+function upload(response,postData) {
+    console.log("You visit /upload");
+    exec("",function(error, stdout, stderr){
+      response.writeHead(200, {"Content-Type": "text/plain"});
+      response.write(querystring.parse(postData).text);
+      response.end();
+    });
+
+}
+function show(response) {
+    console.log("Request handler 'show' was called.");
+    fs.readFile("./img/test.png", "binary", function(error, file) {
+        if (error) {
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write(error + "\n");
+            response.end();
+        } else {
+            response.writeHead(200, {"Content-Type": "image/png"});
+            response.write(file, "binary");
+            response.end();
+        }
+    });
+}
+
+exports.start = start;
+exports.upload = upload;
+exports.show = show;
